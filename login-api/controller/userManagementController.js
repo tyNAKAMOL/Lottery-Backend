4b92809f5419470d76560e9807799367f60c5933
tyNAKAMOL <69255127+tyNAKAMOL@users.noreply.github.com>
2022-03-23 00:10:17 +0700

    UpdateAccount,getSellerAccount, getBankSeller

--- login-api/controller/userManagementController.js
+++ login-api/controller/userManagementController.js
@@ -139,7 +139,8 @@ const register = (req, res, next) => {
                             message: "Internal Server : " + error,
                           });
                           return;
-                        }}
+                        }
+                      }
                     );
                   }
                 }
@@ -224,40 +225,243 @@ const logout = (req, res) => {
   }
 };
 
-// const insertAccount = (req, res) => {
-//   try {
-//     const decoded = jwt.verify(req.body.token, secret);
-//     const { username } = decoded;
-//     let Role = "";
-//     connectionUser.execute(
-//       "SELECT * FROM user WHERE Username=?",
-//       [username],
-//       function (err, users) {
-//         if (err) {
-//           res.json({ status: "500IS", message: "Internal Server : " + err });
-//           return;
-//         }
-//         Role = users[0].Role;
-//       }
-//     );
-//     if (Role == "seller") {
-//       connectionCustomer.execute(
-//         "INSERT INTO customer_account (Storename,Bank_Account,Bank_Account_Number,URL_image_profile) VALUES (?,?,?,?)",
-//         [
-//           req.body.Storename,
-//           req.body.Bank_Account,
-//           req.body.Bank_Account_Number,
-//           req.body.URL_image_profile,
-//         ]
-//       );
-//     }
-//   } catch (error) {
-//     res.json({ status: "500IS", message: "Internal Server : " + error });
-//   }
-// };
+const sellerUpdateAccount = (req, res) => {
+  try {
+    const decoded = jwt.verify(req.body.token, secret);
+    const { username } = decoded;
+    // var Role_ = "";
+    console.log(req.params.action);
+    connectionUser.execute(
+      "SELECT * FROM user WHERE Username=?",
+      [username],
+      function (err, users) {
+        if (err) {
+          res.json({ status: "500IS", message: "Internal Server : " + err });
+          return;
+        }
+        if (users.length == 0) {
+          res.json({ status: "200NF", message: "No User Found" });
+          return;
+        } else {
+          // console.log(users[0].Role);
+          // Role_ += users[0].Role;
+          if (req.params.action == "ChangeBankInfo") {
+            connectionCustomer.execute(
+              "UPDATE seller_account SET Bank_Account=?,Bank_Account_Number=? WHERE Username=? ",
+              [req.body.Bank_Account, req.body.Bank_Account_Number, username],
+              function (err) {
+                if (err) {
+                  res.json({
+                    status: "500IS",
+                    message: "Internal Server : " + err,
+                  });
+                  return;
+                }
+                res.json({
+                  status: "200OK",
+                  message: "Seller update bank success",
+                });
+              }
+            );
+          } else if (req.params.action == "ChangeImage") {
+            connectionCustomer.execute(
+              "UPDATE seller_account SET URL_image_profile=? WHERE Username=? ",
+              [req.body.URL_image_profile, username],
+              function (err) {
+                if (err) {
+                  res.json({
+                    status: "500IS",
+                    message: "Internal Server : " + err,
+                  });
+                  return;
+                }
+                res.json({
+                  status: "200OK",
+                  message: "Seller update URLImage success",
+                });
+              }
+            );
+          } else if (req.params.action == "ChangeAccountInfo") {
+            connectionCustomer.execute(
+              "UPDATE customer_account SET Title=?,Firstname=?,Lastname=?,Email=?,Birthday=?,Tel=?,HomeNo=?,Soi=?,Road=?,Subdistrict=?,District=?,Province=?,ZipCode=? WHERE Username=?",
+              [
+                req.body.Title,
+                req.body.Firstname,
+                req.body.Lastname,
+                req.body.Email,
+                req.body.Birthday,
+                req.body.Tel,
+                req.body.Address.HomeNo,
+                req.body.Address.Soi,
+                req.body.Address.Road,
+                req.body.Address.Subdistrict,
+                req.body.Address.District,
+                req.body.Address.Province,
+                req.body.Address.ZipCode,
+                username,
+              ],
+              function (err) {
+                if (err) {
+                  res.json({
+                    status: "500IS",
+                    message: "Internal Server : " + err,
+                  });
+                  return;
+                } else {
+                  connectionCustomer.execute(
+                    "UPDATE seller_account SET Storename=? WHERE Username=? ",
+                    [req.body.Storename, username],
+                    function (err) {
+                      if (err) {
+                        res.json({
+                          status: "500IS",
+                          message: "Internal Server : " + err,
+                        });
+                        return;
+                      }
+                      res.json({
+                        status: "200OK",
+                        message: "Seller update account success",
+                      });
+                    }
+                  );
+                }
+              }
+            );
+          }
+        }
+      }
+    );
+  } catch (error) {
+    res.json({ status: "500IS", message: "Internal Server : " + error });
+  }
+};
+
+const getSellerAccount = (req, res) => {
+  try {
+    const decoded = jwt.verify(req.params.token, secret);
+    const { username } = decoded;
+    connectionCustomer.execute(
+      "select * from customer_account where username=?",
+      [username],
+      function (err, result) {
+        if (err) {
+          res.json({
+            status: "500IS",
+            message: "Internal Server : " + err,
+          });
+          return;
+        } else if (result.length == 0) {
+          res.json({
+            status: "200NF",
+            message: "username not found in customer account",
+          });
+        } else {
+          connectionCustomer.execute(
+            "select * from seller_account where username=?",
+            [username],
+            function (err, result_) {
+              if (err) {
+                res.json({
+                  status: "500IS",
+                  message: "Internal Server : " + err,
+                });
+                return;
+              } else {
+                let seller = {
+                  Firstname: result[0].Firstname,
+                  Lastname: result[0].Lastname,
+                  Tel: result[0].Tel,
+                  Birthday: result[0].Birthday,
+                  Email: result[0].Email,
+                  Address: {
+                    HomeNo: result[0].HomeNo,
+                    Soi: result[0].Soi,
+                    Road: result[0].Road,
+                    Subdistrict: result[0].Subdistrict,
+                    District: result[0].District,
+                    Province: result[0].Province,
+                    ZipCode: result[0].ZipCode,
+                  },
+                  Storename: result_[0].Storename,
+                  URLImage: result_[0].URL_image_profile
+                };
+                res.json({
+                  status: "200OK",
+                  message: "get data account Success",
+                  sellenAccount: seller,
+                });
+              }
+            }
+          );
+        }
+      }
+    );
+  } catch (error) {
+    res.json({
+      status: "500IS",
+      message: "Internal Server : " + error,
+    });
+  }
+};
+
+const getBankSeller = (req,res) => {
+  try{
+    const decoded = jwt.verify(req.params.token, secret);
+    const { username } = decoded;
+    connectionCustomer.execute(
+      "select * from customer_account where username=?",
+      [username],
+      function (err, result) {
+        if (err) {
+          res.json({
+            status: "500IS",
+            message: "Internal Server : " + err,
+          });
+          return;
+        } else if (result.length == 0) {
+          res.json({
+            status: "200NF",
+            message: "username not found in customer account",
+          });
+        } else {
+          connectionCustomer.execute(
+            "select * from seller_account where username=?",
+            [username],
+            function(err,result){
+              if (err) {
+                res.json({
+                  status: "500IS",
+                  message: "Internal Server : " + err,
+                }); 
+            }
+            else{
+              let sellerBank = {
+                Bank_Account : result[0].Bank_Account,
+                Bank_Account_Number : result[0].Bank_Account_Number,
+              };
+              res.json({
+                status: "200OK",
+                message: "get Seller Bank Success",
+                sellenBack: sellerBank ,
+              });
+            }
+          }
+            );
+        }});
+  }catch (error) {
+    res.json({
+      status: "500IS",
+      message: "Internal Server : " + error,
+    });
+  }
+}
 
 module.exports = {
   login,
   register,
   logout,
+  sellerUpdateAccount,
+  getSellerAccount,
+  getBankSeller,
 };
--- login-api/routes/userManagementRoutes.js
+++ login-api/routes/userManagementRoutes.js
@@ -3,13 +3,19 @@ const {
   login,
   register,
   logout,
+  sellerUpdateAccount,
+  getSellerAccount,
+  getBankSeller
 } = require("../controller/userManagementController");
 
 const router = express.Router();
 
+router.get("/getCustomerAccount/:token",getSellerAccount)
+router.get("/getSellerBank/:token",getBankSeller);
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+router.put("/updateAccount/seller/:action",sellerUpdateAccount);
 
 module.exports = {
   routes: router,
