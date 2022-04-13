b2883fe2a1ca8997cea45a3740387ed246a9befa
tyNAKAMOL <69255127+tyNAKAMOL@users.noreply.github.com>
2022-04-14 02:13:02 +0700

    payment

--- Lottery/controller/adminManagementController.js
+++ Lottery/controller/adminManagementController.js
@@ -140,7 +140,7 @@ const updateStatusSeller = async (req, res) => {
               Subject: "บัญชีของคุณได้รับการยืนยันตัวตน",
               Detail:
                 "ทางเราได้ทำการตรวจสอบการยืนยันตัวจนบัญชีของคุณ" +
-                (await getName(req.body.sellerID)) +
+                (await getName(req.body.sellerID,"seller","SID")) +
                 "เรียบร้อยแล้ว บัญชีของคุณได้รับการอนุมัติสามารถวางจำหน่ายสินค้าได้ ขอขอบคุณ",
               Date: moment(new Date()).format("YYYYMMDDHHmmssZZ"),
               CID: "",
@@ -150,7 +150,7 @@ const updateStatusSeller = async (req, res) => {
             await addTransactionAdmin({
               Event:
                 "Approved SellerAccount [ " +
-                (await getName(req.body.sellerID)) +
+                (await getName(req.body.sellerID,"seller","SID")) +
                 " sellerID : " +
                 req.body.sellerID +
                 "]",
@@ -163,7 +163,7 @@ const updateStatusSeller = async (req, res) => {
               Subject: "บัญชีของคุณไม่ได้รับการยืนยันตัวตน",
               Detail:
                 "ทางเราได้ทำการตรวจสอบการยืนยันตัวจนบัญชีของคุณ" +
-                (await getName(req.body.sellerID)) +
+                (await getName(req.body.sellerID,"seller","SID")) +
                 "เรียบร้อยแล้ว ไม่สามารถอนุมัติให้ใช้งานได้เนื่องจากรูปถ่ายของคุณมีความไม่ชัดเจน/ไม่ถูกต้อง/ไม่สามารถระบุตัวตนได้ กรุณาส่งรูปถ่ายยืนยันตัวตนอีกครั้ง หากมีข้อสงสัยติดต่อที่ admin ขอขอบคุณ",
               Date: moment(new Date()).format("YYYYMMDDHHmmssZZ"),
               CID: "",
@@ -173,7 +173,7 @@ const updateStatusSeller = async (req, res) => {
             await addTransactionAdmin({
               Event:
                 "Reject SellerAccount [ " +
-                (await getName(req.body.sellerID)) +
+                (await getName(req.body.sellerID,"seller","SID")) +
                 " sellerID : " +
                 req.body.sellerID +
                 "]" + " Because Image cannot Identity.",
@@ -195,6 +195,146 @@ const updateStatusSeller = async (req, res) => {
     });
   }
 };
+
+const getOrderPayment = async (req, res) => {
+  try {
+    let order = [];
+    let validateData = {
+      token: req.params.token,
+    };
+    const errMsg = validateMethod(validateData);
+    if (errMsg.length > 0) {
+      res.json({
+        status: "403MP",
+        message: "Missing or Invalid Parameter : " + errMsg,
+      });
+      return;
+    } else {
+      const decoded = jwt.verify(req.params.token, secret);
+      const { username, role } = decoded;
+      if (role == "admin") {
+        const [result] = await promiseOrder.execute(
+          "select * from order_c where Status = 'Audit Payment'"
+        );
+        if (result.length == 0) {
+          res.json({
+            status: "200NF",
+            message: "not found in order status Audit Payment",
+          });
+        } else {
+          for (let i = 0; i < result.length; i++) {
+            order.push({
+              orderID: result[i].OID,
+              customerID: result[i].CID,
+              FullName: await getName(result[i].CID,"customer","CID"),
+              URLSlip: result[i].URLSlip,
+              Money: result[i].Money,
+            });
+          }
+          res.json({
+            status: "200OK",
+            message: "get orderPayment success!!",
+            orderPayment : order,
+          });
+        }
+      } else {
+        res.json({
+          status: "401UR",
+          message: "Unauthorized",
+        });
+      }
+    }
+  } catch (error) {
+    res.json({
+      status: "500IS",
+      message: "Internal Server : " + error,
+    });
+  }
+};
+const updateOrderPayment = async (req, res) => {
+  try {
+    let validateData = {
+      token: req.body.token,
+      approve: req.body.approve, 
+      money: req.body.Money, 
+    };
+    const errMsg = validateMethod(validateData);
+    if (errMsg.length > 0) {
+      res.json({
+        status: "403MP",
+        message: "Missing or Invalid Parameter : " + errMsg,
+      });
+      return;
+    } else {
+      const decoded = jwt.verify(req.body.token, secret);
+      const { username, role } = decoded;
+      const [adminID] = await promiseAdmin.execute(
+        "SELECT AID FROM account WHERE Username=?",
+        [username]
+      );
+      if (adminID != undefined) {
+        if (role == "admin") {
+          if (req.body.approve == "Yes") {
+            await promiseOrder.execute(
+              "UPDATE order_c SET Status=? WHERE OID=? and CID=? and Status='Audit Payment' ",
+              ["Seller Check Order", req.body.orderID,req.body.customerID]
+            );
+            await sendInbox({
+              Subject: "ตรวจสอบรายการคำสั่งซื้อที่ " + req.body.orderID ,
+              Detail: "กรุณาตรวจสอบคำสั่งซื้อที่ " + req.body.orderID + "ที่หน้าตรวจสอบคำสั่งซื้อ และกดปุ่มยืนยันเพื่อทำการยืนยันคำสั่งซื้อ หรือกดปุ่มยกเลิกเพิ้อทำการยกเลิกคำสั่งซื้อ ขอขอบคุณ",
+              Date: moment(new Date()).format("YYYYMMDDHHmmssZZ"),
+              CID: "",
+              SID: req.body.sellerID,
+              AID: adminID[0].AID,
+            });
+            await addTransactionAdmin({
+              Event:
+                "Approved Payment Order: [ " + req.body.orderID + " ]",
+              actionDate: moment(new Date()).format("YYYYMMDDHHmmssZZ"),
+              AID: adminID[0].AID,
+            });
+          
+          } else {
+            await promiseOrder.execute(
+              "UPDATE order_c SET URLSlip='', Status='Pending Payment' WHERE Status='Audit Payment' and OID=?",
+              [req.body.OrderID]
+            );
+            //....
+            await sendInbox({
+              Subject: "คำสั่งซื้อที่ "+ req.body.orderID + "ชำระเงินไม่สำเร็จ",
+              Detail:
+                "ทางเราได้ทำการตรวจสอบหลักฐานการชำระเงินของคุณ " +
+                (await getName(req.body.customerID,"customer","CID")) +
+                " ทางเราไม่สามารถอนุมัติหลักฐานการชำระเงินได้เนื่องจากหลักฐานการชำระเงินรูปภาพไม่ชัดเจนหรือชำระเงินไม่ถูกต้อง กรุณาชำระเงินให้ครบจำนวนเงิน/ส่งหลักฐานการชำระเงินใหม่ หากมีข้อสงสัยติดต่อที่ Admin ขอขอบคุณ",
+              Date: moment(new Date()).format("YYYYMMDDHHmmssZZ"),
+              CID: req.body.customerID,
+              SID: "",
+              AID: adminID[0].AID,
+            });
+            await addTransactionAdmin({
+              Event:
+                "Reject Payment Order [ " +
+                req.body.orderID +
+                "]" + " Because customer payment incompleted.",
+              actionDate: moment(new Date()).format("YYYYMMDDHHmmssZZ"),
+              AID: adminID[0].AID,
+            });
+          }
+          res.json({
+            status: "200OK",
+            message: "Success",
+          });
+        }
+      }
+    }
+  } catch (error) {
+    res.json({
+      status: "500IS",
+      message: "Internal Server : " + error,
+    });
+  }
+};
+
 const sendInbox = async (params) => {
   await promiseAdmin.execute(
     "INSERT INTO inbox (Subject,Detail,Date,CID,SID,AID) VALUES (?,?,?,?,?,?)",
@@ -208,6 +348,7 @@ const sendInbox = async (params) => {
     ]
   );
 };
+
 const addTransactionAdmin = async (params) => {
   await promiseAdmin.execute(
     "INSERT INTO transaction (Event,actionDate,AID) VALUES (?,?,?)",
@@ -215,10 +356,10 @@ const addTransactionAdmin = async (params) => {
   );
 };
 
-const getName = async (SID) => {
+const getName = async (ID,role,key) => {
   const [FullName] = await promiseCustomer.execute(
-    "SELECT Firstname,Lastname FROM seller_account WHERE SID=?",
-    [SID]
+    "SELECT Firstname,Lastname FROM "+ role +"_account WHERE "+ key +"=?",
+    [ID]
   );
   let fullName = FullName[0].Firstname + " " + FullName[0].Lastname;
   return fullName;
@@ -227,4 +368,5 @@ const getName = async (SID) => {
 module.exports = {
   getSellerIdentity,
   updateStatusSeller,
+  getOrderPayment
 };
--- Lottery/routes/adminManagementRoutes.js
+++ Lottery/routes/adminManagementRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const {
     getSellerIdentity,
-    updateStatusSeller
+    updateStatusSeller,
+    getOrderPayment
 } = require("../controller/adminManagementController");
 
 const router = express.Router();
 
 router.get("/getSellerIdentity/:token", getSellerIdentity);
+router.get("/getOrderPayment/:token", getOrderPayment);
 router.put("/updateSellerIdentity",updateStatusSeller);
 
 module.exports = {
